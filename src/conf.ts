0430484a2e7a11a00590e7326200136ae8263d15
Gilsuk <devgilsuk@gmail.com>
2020-05-12 17:05:48 +0900

    link endpoint for playground

--- src/conf.ts
+++ src/conf.ts
@@ -4,6 +4,15 @@ const conf = {
   schema: schema,
   context: context,
   rootValue: rootValue,
+  playground: {
+    tabs: [
+      {
+        endpoint:
+          "https://5mt1j7nzw1.execute-api.ap-northeast-2.amazonaws.com/prod/graphql",
+        query: "## Open docs for query example",
+      },
+    ],
+  },
 };
 
 export default conf;
